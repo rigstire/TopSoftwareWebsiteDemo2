af46fec989c112a0ec9385600a23f3ab6bbdbd9e
rigstire <brodsky.nick@gmail.com>
2025-01-22 10:25:05 -0800

    Fix: Add gulp to dependencies

--- Backend/TopSoftware/TopSoftware/settings.py
+++ Backend/TopSoftware/TopSoftware/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [    'localhost',
     '127.0.0.1',
-    'testserver',]
+    'testserver',
+    'topsoftware.herokuapp.com']
 
 
 # Application definition
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn topsoftware.wsgi --log-file -
