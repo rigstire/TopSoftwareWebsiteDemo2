8786afca5e7b53593e576ce9ea683a62379398b8
rigstire <brodsky.nick@gmail.com>
2025-01-24 14:48:51 -0800

    fix module

--- Backend/TopSoftware/wsgi.py
+++ Backend/TopSoftware/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TopSoftware.TopSoftware.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TopSoftware.TopSoftware.settings.py')
 
 application = get_wsgi_application()
