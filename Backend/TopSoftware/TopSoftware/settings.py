2b2ad62f02714546cfb49e297b101f5fd09da7df
rigstire <brodsky.nick@gmail.com>
2025-01-24 13:15:12 -0800

    waitress

--- Backend/TopSoftware/TopSoftware/settings.py
+++ Backend/TopSoftware/TopSoftware/settings.py
@@ -153,3 +153,4 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'officialtopsoftware@gmail.com'
 EMAIL_HOST_PASSWORD = 'awcd wuxq qkzm dajb' 
+ASGI_APPLICATION = "your_project.asgi.application"
