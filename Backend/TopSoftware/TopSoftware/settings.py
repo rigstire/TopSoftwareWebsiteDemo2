7851c4356b11514ae8a8b8bb69d1e5e1198bed34
rigstire <brodsky.nick@gmail.com>
2025-01-24 14:22:27 -0800

    commit

--- Backend/TopSoftware/TopSoftware/settings.py
+++ Backend/TopSoftware/TopSoftware/settings.py
@@ -154,3 +154,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'officialtopsoftware@gmail.com'
 EMAIL_HOST_PASSWORD = 'awcd wuxq qkzm dajb' 
 ASGI_APPLICATION = "your_project.asgi.application"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
