3ceae2ae326ef79305f4e03ecf9888deb2d34441
rigstire <brodsky.nick@gmail.com>
2025-01-24 15:22:12 -0800

    __init__.py

--- Backend/TopSoftware/TopSoftware/wsgi.py
+++ Backend/TopSoftware/TopSoftware/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TopSoftware.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TopSoftware.TopSoftware.settings')
 
 application = get_wsgi_application()
